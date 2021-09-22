ba6f9b713f28739b9e876a62790af0dcac5b1937
Arman Habib <armanadd01@gmail.com>
2021-09-23 04:17:04 +0600

    new commit

--- src/components/Shops/Shops.js
+++ src/components/Shops/Shops.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Shops.css'
 
-const shops = () => {
+const Shops = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{
+        fetch('./products.JSON')
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    },[])
     return (
-        <div>
-            <h1>This is shops</h1>
+        <div className="shop-container">
+            <div className="product-container">
+                <h3>Total Products: {products.length} </h3>
+            </div>
+            <div className="cart-container">
+                <h3>this is cart section</h3>
+            </div>
+            
         </div>
     );
 };
 
-export default shops;
+export default Shops;
