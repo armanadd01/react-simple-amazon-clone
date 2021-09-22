32f3e2fff6b23c9f0e72741b0fc86f04d5b055b3
Arman Habib <armanadd01@gmail.com>
2021-09-23 03:24:22 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Headers from './components/Headers/Headers';
+import Shops from './components/Shops/Shops';
 
 function App() {
   return (
     <div className="">
       <Headers></Headers>
+      <Shops></Shops>
     </div>
   );
 }
---
+++ src/components/Shops/Shops.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const shops = () => {
+    return (
+        <div>
+            <h1>This is shops</h1>
+        </div>
+    );
+};
+
+export default shops;
